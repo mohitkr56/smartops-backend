2d18d125c01e8bcc0a4906bc3c4562c5a644de2e
ANAND KUMAR SAHA <113061571+mohitkr56@users.noreply.github.com>
2026-02-20 20:29:02 +0530

    Initial commit

---
+++ src/main/java/com/smartops/smartops_backend/SmartopsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartops.smartops_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartopsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartopsBackendApplication.class, args);
+	}
+
+}
